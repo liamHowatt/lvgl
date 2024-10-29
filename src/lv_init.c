65a8c4b523bc315b9bec1a0895149643057867d5
liamHowatt <liamjmh0@gmail.com>
2024-10-29 11:05:00 +0100

    test

--- src/lv_init.c
+++ src/lv_init.c
@@ -468,6 +468,13 @@ void lv_deinit(void)
 
 }
 
+#include "draw/lv_draw_line.h"
+void lv_foo(void)
+{
+    lv_draw_line_dsc_t dsc = {.blend_mode = LV_BLEND_MODE_MULTIPLY};
+    LV_UNUSED(dsc);
+}
+
 /**********************
  *   STATIC FUNCTIONS
  **********************/
